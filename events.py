9f0ddcdc57215f94dfb5d46402491637890b45cf
Russ Haun <russhaun@verizon.net>
2018-09-18 00:44:38 -0400

    events.py updates:
    Added install/uninstall dll functions to events.py. prepping for merge to testing branch

--- events.py
+++ events.py
@@ -47,10 +47,10 @@ token = OpenProcessToken(process, TOKEN_READ)
 my_sid = GetTokenInformation(token, TokenUser)[0]
 myType = win32evtlog.EVENTLOG_INFORMATION_TYPE
 #Left this here for reference
-"""Report an event for a previously added event source."""
 #ReportEvent(appName, eventID, eventCategory = 0, eventType=win32evtlog.EVENTLOG_ERROR_TYPE, strings = None, data = None, sid=None):
 #
 #below are as defined in the "dll" look @ included .mc file for message contents
+#
 #descr = 'ARTILLERY_START'
 def ArtilleryStartEvent():
         eventID = 100
@@ -67,20 +67,33 @@ def HoneyPotEvent(ip):
         eventID = 200
         myType = win32evtlog.EVENTLOG_WARNING_TYPE
         ReportEvent(AppName, eventID, eventCategory=category, eventType=myType, strings=HPip, data=data, sid=my_sid)
+#
 #below functions are not implemented yet. these will be included in setup.py when they are
 def InstallDLL():
-        """Add a source of messages to the event log."""
-        #AddSourceToRegistry(appName = AppName, msgDLL = mymsgDLL, eventLogType = "Application", eventLogFlags = None):
-        pass
+        """Adds Artillery as a source of messages to the windows event log."""
+        # Have to pass path var here if i use true path ex: "c:\path\to\dll" 
+        # it fails when logging event for some reason.
+        mymsgDLL = '%PROGRAMFILES(x86)%\\Artillery\\src\\windows\\ArtilleryEvents.dll'
+        AddSourceToRegistry(appName = AppName, msgDLL = mymsgDLL, eventLogType = "Application", eventLogFlags = None)
+        #have to write 2 more keys function only does 1 . working on function for it
+        #path to key in registry
+        #Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EventLog\Application\Artillery
+        #write an event
+        #DllInstalled()       
 #
 #
 def UninstallDLL():
-        """Removes a source of messages from the event log."""
-        #RemoveSourceFromRegistry(appName = AppName, eventLogType = "Application"):
-        pass
-
+        """Removes Artillery as a source of messages from the windows event log."""
+        # fire uninstall event and wait a sec before removing 
+        # to make sure messege gets logged since we are removing dll
+        #DllRemoved()
+        #time.sleep(5)
+        RemoveSourceFromRegistry(appName = AppName, eventLogType = "Application")
+#
 #
 #these are used during testing leave commented for now
+#UninstallDLL()
+#InstallDLL()
 #ArtilleryStartEvent()
 #ArtilleryStopEvent()
 #ip='127.0.0.1'
